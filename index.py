5b7b971e2107943a297e2b2bcc126a5133566333
Venkata Naga Jostna Padarthi <jyosthnapadarthi22@gmail.com>
2025-10-27 21:33:22 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.title("Shreyas Iyer")
+st.subheader("Crickter")
+st.divider()
+st.text("""Shreyas Santosh Iyer (born 6 December 1994) is an Indian international cricketer who plays for the Indian team as a right-handed middle-order batter. He is currently the vice captain of the Indian cricket team in the ODI format. He also plays for Mumbai in domestic cricket and captains the Punjab Kings in the Indian Premier League. He was a part of the Indian team that won the 2025 Champions Trophy.
+        """)
+st.divider()
+st.markdown("[ICC mens player](https://web.archive.org/web/20250416070518/https://timesofindia.indiatimes.com/sports/cricket/news/shreyas-iyer-wins-icc-mens-player-of-the-month-award/articleshow/120305645.cms)")
+st.markdown("[KOLKOTA KNIGHT RIDDERS](https://www.kkr.in/news/shreyas-iyer-returns-as-captain-of-kkr-nitish-rana-named-vice-captain)")
+st.markdown("[KOLKOTA KNIGHT RIDDERS CAPTAIN](https://www.espncricinfo.com/story/ipl-2022-kkr-shreyas-iyer-named-kolkata-knight-riders-new-captain-1301426)")
+
+
+
